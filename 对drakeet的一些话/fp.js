9645bc70b8a21c4f4ceafc4aeb4cad3d53b12f8b
duangsuse <fedora-opensuse@outlook.com>
2019-08-01 22:34:58 +0800

    FP.js: Fix dropWhile, add combination...#

--- 对drakeet的一些话/fp.js
+++ 对drakeet的一些话/fp.js
@@ -40,6 +40,7 @@ is.funamed = function funamed(name) {
   return function fjudge(f) { return is.fun(f) && f.name === name; }; };
 is.empty = function isempty(x) { return x.length === 0; };
 is.exist = function exists(name) { return is.defined(toplevelThis()[name]); };
+is.none = function isNone(x) { return is.null(x) || is.undef(x); };
 
 function toplevelThis() {
   var bad = function() { return Function('return this')(); }
@@ -140,6 +141,7 @@ is.single = oget.curry1('length').andThen(fulleq.curry1(1));
 is.defined = is.undef.andThen(not);
 is.nund = is.und.andThen(not);
 is.nm1 = is.m1.andThen(not);
+is.notfun = is.fun.andThen(not);
 var mix = bound(Object, 'assign').curriedN(1);
 var globalThis = globalThis || toplevelThis();
 
@@ -378,9 +380,11 @@ function takeWhile(p, xs) {
 }
 // Why not stream? define dropWhile as collector will save my time...
 function dropWhile(p, xs) {
-  xs = tryLookAhead1Iter(xs);
-  var x; while (!(x = nextProtect(xs)).done && p(x.next)) {}
-  return collect(xs);
+  xs = tryLookAhead1Iter(xs); var ary = [];
+  if (!p((x = nextProtect(xs)).value)) return ary;
+  var x; do { ary.push(x.value); } while (!(x = nextProtect(xs)).done && p(x.next));
+  ary.push(x.value);
+  return ary;
 }
 
 function zipWith(f, xs, ys) {
@@ -520,34 +524,253 @@ function orderedLT(xs) {
   return foldl(and, true, ps);
 }
 
+function makeCats(items) {
+  return foldn.curry2(function(xs) {
+    return join([joinMap(function(x) { return map(
+      function(c) { return c+x; }, items); }, xs), xs]); }, items);
+}
+
+function lazyCats(items) { var xs = items;
+return function cats(n) {
+  return join([xs, joinMap(function(x)
+    { return map(function(c) { return c+x; }, items); },
+      (n !== 0)? cats(--n) : konst(_done()))]);
+}; }
+
 //// Simple Left-Right recursive(1) Parser Combinator
 function chars(str) {
-  if (Symbol && Symbol.iterator &&
-    String.prototype[Symbol.iterator]) return [...str];
-  return str.split(''); }
+  if (str instanceof Array) return str;
+  var iter; if (Symbol && Symbol.iterator &&
+    (iter = String.prototype[Symbol.iterator]))
+      return bound(iter.bind(str)(), 'next');
+  return tryIter(str.split('')); }
 
-function makeNew(ctor) {}
+function Feeder(charz, fname) {
+  this.stream = tryLookAhead1Iter(is.string(charz)? chars(charz) : charz);
+  this.count = 0; this.col = 0;
+  this.lineNum = 1; this.scanCRLF = false;
+  this.fileName = fname || '<feed>';
+  this.lastItem = null; this.nextItem = null;
+  this.userContext = {};
+  this.stack = null;
+  this.saveCount = 0;
+}
+function _eof(s) { return (is.undef(s))? "<eof>" : (is.null(s))? "<begin>" : "`"+s+"'"; }
+function _sp(s) { var c = s.charAt(0); return (c == ' ' || c == ':')? s : ' '+s; }
+Feeder.prototype.lastIsLF = function isLineFeed()
+  { return this.lastItem === '\n'; };
+Feeder.prototype.lastIsCR = function isCarriageReturn()
+  { return this.lastItem === '\r'; };
+Feeder.prototype.lastIsCRLF = function isCRLF()
+  { return (this.nextItem === '\n' && this.lastItem === '\r'); };
 
-function seq(ps, folder, msgr) {}
-function lookahead1(pmap, flmap, msgr) {}
+Feeder.prototype.duplast = function dupLastItem() { ++this.saveCount; };
+Feeder.prototype.reset1Tho = function dupLastOnce(r) { this.saveCount = 1; return r; };
+Feeder.prototype.next = function nextItem() {
+  if (this.saveCount !== 0) { --this.saveCount; return this.lastItem; }
+  var x; if ((x = this.stream()).done) { return (this.lastItem= undefined); }
+  this.lastItem = x.value; this.nextItem = x.next; var p;
+  if ((p = (this.lastIsCRLF() && (this.scanCRLF = true))  )
+    || this.lastIsCR() // carriage return is ok immediately
+    ||(!this.scanCRLF && this.lastIsLF()) // skip one '\n'
+    ||((this.scanCRLF && this.lastIsLF()) && (this.scanCRLF = false)) )
+    { ++this.lineNum; this.col = p? (-2)/*for next <nl>*/ : (-1); }
+  this.count += 1; this.col += 1;
+  return this.lastItem; };
+Feeder.prototype.nextNext = function next2() { this.next(); return this.nextItem; };
+Feeder.prototype.eof = function lastEOF() { return is.undef(this.lastItem); };
+Feeder.prototype.hasNext = function nextEOF()
+  { return !is.undef(this.nextItem) || this.count === 0; };
+Feeder.prototype.loc = function sourceLocation()
+  { return this.fileName+':'+this.lineNum+':'+this.col; };
+Feeder.prototype.desc = function describe(prefix)
+  { return this.loc() +(prefix? prefix : ' ')
+    +"(pos="+this.count+'@'+_eof(this.lastItem)+(this.nextItem? ", next=`"+this.nextItem+"')" : ")"); };
+Feeder.prototype.logs = function pushStack(x)
+  { if (!is.null(this.stack)) this.stack.push(x); return this; };
+Feeder.prototype.leave = function popStack()
+  { if (!is.null(this.stack)) this.stack.pop(); return this; };
 
-function someFold(p, folder, msgr) {}
-function manyFold(p, folder, msgr) {}
+function makeNew(ctor) {
+  return function constructNew(matches) {
+    var ths = new ctor; return ctor.bind(ths).apply(ctor, matches); }; }
+///
+function parsed(xs) { return xs[0]; }
+function presult(xs) { return xs[1]; }
+function perror(xs) { return xs[1]; }
 
-function chain1LeftRec() {}
-function chain1RightRec() {}
+function pmatch(v) { return [true, v]; }
+function pfail(m) { return [false, is.undef(m)? '' : m]; }
 
-function satisfy(predicate) {}
+//
+function seq(ps, folder, msgr) { // 不好意思... 这个流组合子的架构有点过分了...
+if (!is.fun(msgr)) msgr = function failed(f, r, i, err) // 好像很多子程序都得手动指定自己消不消耗字符
+  { return "Parser failed @"+f.desc()+", sequence item "+i+": " + err; };
+if (!is.fun(folder)) folder = makeNew(Array);
+return function sequential(feeder) {
+  var rs = [], i = 0, fail = null;
+  foreach(ps)(function(parser) {
+    var result = parser(feeder);
+    if (parsed(result)) { rs.push(presult(result)); }
+    else { var msg = msgr(feeder, rs, i, perror(result));
+      fail = msg; throw breakIter; }
+    i += 1; feeder.next(); // seq next
+  });
+  if (!is.null(fail)) {
+    if (fail instanceof Error) { throw fail; }
+    else return pfail(fail); }
+  return pmatch(folder(rs));
+}; }
 
-function kwP(str) {}
-function elemP(iset) {}
+// 想想看 seq 的方式和 possible 的方式还可能是无法调节，
+// 重置，可是如果是在 possible 里，如果让 possible 处理这个不一致则会显得莫名其妙
+// （而且很难看，因为目标是 next，如果一个成功什么都不做、如果一个失败你还得 next 一下，莫名其妙）
+// 我还是决定.... 不要 possible 算了，possible 的用户都自己写。
+function possible(ps, msgr) {
+if (!is.fun(msgr)) msgr = function failedBr(f, r, i)
+  { return "All parser branches have failed @"+f.desc()
+    +", while matching item #"+i+": " + r[i][1]; };
+return function branches(feeder) {
+  var i = -1, fails = [], accepted = null;
+  foreach(ps)(function(branch) {
+    var result = branch(feeder);
+    if (parsed(result)) { accepted = presult(result); throw breakIter; }
+    else { i += 1; fails.push([i, perror(result)]); throw nextIter; }
+  });
+  if (is.null(accepted)) {
+    var msg = msgr(feeder, fails, i);
+    if (msg instanceof Error) throw msg;
+    else return pfail(msg); }
+  return pmatch(accepted);
+}; }
 
-var ws = " \t\n\r"
+function lookahead1(pmap, flmap, msgr) {
+if (!is.fun(msgr)) msgr = function failedLookAhead(f, n, i)
+  { return "Lookahead1: unexpected item "+_eof(n)+", expecting one of ["
+    +Object.keys(pmap).join(', ')+"] item"; };
+return function prefetch1(feeder) {
+  var ahead1 = feeder.nextItem;
+  if (is.object(pmap)) {
+    if (!(ahead1 in pmap)) {
+      var msg = msgr(feeder, ahead1);
+      if (msg instanceof Error) throw msg;
+      else return pfail(msg); }
+    return pmatch(pmap[ahead1](ahead1));
+  } else { // is ahead-looking predicates
+    var match = null, i = 0; // 迫真 i
+    foreach(pmap)(function(case1) {
+      if (case1(ahead1)) { match = flmap[case1.name]; throw breakIter; }
+      i += 1; });
+    if (is.null(match)) { var msg = msgr(feeder, ahead1, i);
+      if (msg instanceof Error) throw msg;
+      else return pfail(msg); }
+    return pmatch(match(ahead1)); } 
+}; }
 
-function wsP() {}
-function ws0P() {}
+function someFold(p, folder, msgr) { // use it in seq
+if (!is.some(folder)) folder = ['', add];
+if (!is.fun(msgr)) msgr = function failedFold(f, m) {
+  return "Failed fold: "+m; };
+return function foldChain(feeder) { var match, v = folder[0], f = folder[1];
+  match = p(feeder);
+  if (!parsed(match)) { var msg = msgr(feeder, perror(match));
+    if (msg instanceof Error) throw msg;
+    else return pfail(msg); }
+  v = f(v, presult(match)); feeder.next();
+  while (parsed( (match = p(feeder)) )) {
+    v = f(v, presult(match));
+    feeder.next(); }
+  feeder.duplast(); // cancel next next
+  return pmatch(v);
+}; }
+function manyFold(p, folder) { // use it in seq
+if (!is.some(folder)) folder = ['', add];
+return function foldChain0(feeder) { var match, v = folder[0], f = folder[1];
+  match = p(feeder);
+  if (!parsed(match)) { return pmatch(v); }
+  while (parsed( (match = p(feeder)) )) {
+    v = f(v, presult(match));
+    feeder.next(); }
+  feeder.duplast(); // cancel next next
+  return pmatch(v);
+}; }
 
-function run() {}
+function chain1LeftRec(p, f) {
+return function chainRecL(feeder, v) { var lm;
+  lm = p(feeder);
+  if (!parsed(match)) { return pmatch(v); }
+  else return pmatch( chainRecL(feeder, f(v, x)) );
+}; }
+function chain1RightRec(p, f, v) {
+return function chainRecR(feeder) { var lm;
+  lm = p(feeder);
+  if (!parsed(match)) { return pmatch(v); }
+  else return pmatch( f(x, presult(chainRecL(feeder, v))) );
+}; }
+
+function satisfy(predicate, m, fmt) {
+if (!is.fun(fmt)) fmt = function(s, mm) { return mm; };
+return function expectful(feeder) {
+  var it = feeder.lastItem;
+  return (predicate(it))? pmatch(it) : feeder.reset1Tho(pfail(fmt(feeder, m)));
+}; }
+
+function charP(c, m, fmt) {
+if (!is.fun(fmt)) fmt = function (s, mm) { return 'expecting ' + c +_sp(mm); };
+return function expectChar(feeder) {
+  var x = feeder.lastItem;
+  return (x === c)? pmatch(x) : feeder.reset1Tho(pfail(fmt(feeder, m)));
+}; }
+
+var _sindex = String.prototype.codePointAt?
+   function cpAt(s, idx) { return s.codePointAt(idx); }
+  :function chAt(s, idx) { return s.charAt(idx); };
+function kwP(str, m, fmt) {
+if (!is.string(m)) m = '';
+if (!is.fun(fmt)) fmt = function format(s, i)
+  { return (s.eof()? 'Unexpected EOF. ':'') + 'Expecting keyword <'+str+'>@'+i+ _sp(m); };
+return function stringP(feeder) { for (var i = 0, x = feeder.lastItem;
+  i !=str.length && is.string(x) && _sindex(x,0) === _sindex(str, i++); x = feeder.next()) {}
+  feeder.duplast(); // revert sequential skip
+  return (i === str.length)? pmatch(str) : pfail(fmt(feeder, i));
+}; }
+function elemP(iset, m, fmt) {
+if (!is.string(m)) m = '';
+if (!is.fun(fmt)) fmt = function format(s)
+  { return (s.eof()? 'Unexpected EOF. ':'') + 'Expecting one of ['
+    +iset.join(', ')+']' +_sp(m); };
+return function containsP(feeder) {
+  var it = feeder.lastItem;
+  return (iset.includes(it))? pmatch(it) : feeder.reset1Tho(pfail(fmt(feeder)));
+}; }
+
+var ws = " \t\n\r".split('');
+function setWs(wscharz) { ws = wscharz; }
+function wsP(msg, fmt) {
+if (!is.string(msg)) msg = 'at least one whitespace';
+if (!is.fun(fmt)) fmt = function(f){ return msg; };
+return function skipWhite(feeder) {
+  if (!ws.includes(feeder.lastItem)) return feeder.reset1Tho(pfail(fmt(feeder, msg)));
+  var count = 0+1;
+  while (ws.includes(feeder.nextItem)) { feeder.next(); ++count; }
+  return pmatch(count);
+}; }
+function ws0P() {
+return function skipWhiteMaybe(feeder) {
+  if (!ws.includes(feeder.lastItem)) { feeder.duplast(); return pmatch(0); }
+  var count = 0+1;
+  while (ws.includes(feeder.nextItem)) { feeder.next(); ++count; }
+  return pmatch(count);
+}; }
+
+function run(parser) {
+return function runParser(input) {
+  if (!(input instanceof Feeder)) input = new Feeder(input);
+  if (is.null(input.lastItem)) input.next();
+  try { var res = parser(input); }
+  catch (e) { return Either.left(e); }
+  return (parsed(res))? Either.right(presult(res)) : Either.left(perror(res));
+}; }
 
 //// Useful Monads
 // class Functor (c :: * -> *) where id :: forall a. c a a; (.) :: c y z -> c x y -> c x z
@@ -586,17 +809,18 @@ function Maybe(cat) {
 } inherits(Maybe, Monad);
 
 Maybe.NULL = Maybe.of(null);
-Maybe.prototype.isNothing = function isNothing() { return is.null(this.inner); };
-Maybe.prototype.isJust = function isJust() { return !is.null(this.inner); };
+Maybe.prototype.isNothing = function isNothing() { return is.none(this.inner); };
+Maybe.prototype.isJust = function isJust() { return !is.none(this.inner); };
 Maybe.prototype.get = function unwrap()
   { if (this.isJust()) return this.inner; else throw Error('Unwrapping NULL Maybe'); };
+Maybe.prototype.or = function otherMay(that) { return (this.isJust())? this : that ; }
 Maybe.prototype.getOr = function unwrapOr(y)
   { return (this.isJust())? this.get() : y; };
 Maybe.prototype.getOrElse = function unwrapOrElse(pf)
   { return (this.isJust())? this.get() : pf(); };
 
 Maybe.prototype.fmap = function isNullQ(f) {
-  if (is.null(this.inner)) return Maybe.NULL;
+  if (is.none(this.inner)) return Maybe.NULL;
   else return this.eta(f(this.inner));
 };
 Maybe.prototype.toString = function show() { return (this.isJust())?
@@ -663,15 +887,17 @@ module.exports = {
     map, filter, join, joinMap, filterMap,
     takeWhile, dropWhile,
     zipWith, unzip, zipWith3, unzip3, deepIter,
-    iterBy, repeat, infseq, range, orderedLT },
+    iterBy, repeat, infseq, range, orderedLT,
+    combination: makeCats, lazyCombination: lazyCats },
   dim: { allocSize:dimAllocSize, calcPtr:dimGetElemPtr },
   html: { _isLoaded, _waits, 
     logs, helem, merges, cssSelect, waitsId, waitsCss,
     _____, delay, secs,
     append, prepend, xhr },
-  parserc: { chars, makeNew, seq, lookahead1,
+  parserc: { chars, Feeder, makeNew, seq, possible, lookahead1,
+    parsed, pmatch, pfail, presult, perror,
     someFold, manyFold, chain1LeftRec, chain1RightRec,
-    satisfy, kwP, elemP, wsP, ws0P, run },
+    satisfy, charP, kwP, elemP, setWs, wsP, ws0P, run },
   functor: { Functor, Monad, Maybe, Either }
 }; }catch(SyntaxError){}
 
