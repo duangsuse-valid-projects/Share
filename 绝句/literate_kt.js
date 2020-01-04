145739a1f80067cea940ca7cdbb9dd501039dd42
duangsuse <fedora-opensuse@outlook.com>
2020-01-04 23:46:56 +0800

    绝句: literate_kt.js: dependency

--- 绝句/literate_kt.js
+++ 绝句/literate_kt.js
@@ -1,7 +1,17 @@
-function *nextSiblings(e) { for (let c = e; c!=null; c=c.nextSibling) yield c; }
+function nextBy(succ) {
+  return function *(init) {
+    for (let cur = init; succ(cur); cur = succ(cur)) yield cur;
+  };
+}
 function *takeWhile(p, xs) { for (let x of xs) if (p(x)) { yield x; } else break; }
 function negate(p) { return x => !p(x); }
 
+function assignDOMAttribute(node, attributes) {
+  for (let [name, value] of Object.entries(attributes))
+    node.setAttribute(name, value.toString());
+}
+
+nextSiblings = nextBy(x => x.nextSibling);
 const sectionEnd = it => it.classList!=null&&it.classList.contains("literateEnd");
 function filterCode(begin_e, p = e => e.classList.contains("language-kotlin")) {
   let neighbors = nextSiblings(begin_e);
@@ -9,17 +19,38 @@ function filterCode(begin_e, p = e => e.classList.contains("language-kotlin")) {
   return [...section].filter(e => e.classList!=null&&p(e)).map(e => e.innerText).join("");
 }
 
-function createCodeElement(text) {
+function configKotlinPlayground(code) {
+  assignDOMAttribute(code, {
+    "indent": 2,
+    "auto-indent": true,
+    "data-autocomplete": true,
+    "highlight-on-fly": true,
+    "match-brackets": true
+  });
+}
+function createPreCodeElement(text) {
   let pre = document.createElement("pre");
   let code = document.createElement("code"); code.textContent = text;
   pre.appendChild(code); return pre;
+}
+function createTextarea(text) {
+  let textarea = document.createElement("textarea"); textarea.textContent = text;
+  return textarea;
 }
 function enableCodeFilter(begin_e) {
   let end_e = [...nextSiblings(begin_e)].find(sectionEnd);
   let codeDiv = document.createElement("div"); codeDiv.innerHTML = `<button>Kotlin Code</button>`; codeDiv.classList.add("playground");
   begin_e.parentElement.insertBefore(codeDiv, end_e);
+  let dependencyDivs = begin_e.getAttribute("depend"); if (dependencyDivs!=null) dependencyDivs = dependencyDivs.split(" ").map(id => filterCode(document.getElementById(id)));
   let btn = codeDiv.children[0];
-  btn.onclick = () => { codeDiv.appendChild(createCodeElement(filterCode(begin_e))); btn.remove(); KotlinPlayground(codeDiv); };
+  btn.onclick = () => {
+    let pre = createPreCodeElement(filterCode(begin_e)); configKotlinPlayground(pre);
+    if (dependencyDivs!=null) {
+      let dependTa = createTextarea(dependencyDivs.join(""));
+      dependTa.classList.add("hidden-dependency"); pre.firstChild.appendChild(dependTa);
+    }
+    codeDiv.appendChild(pre); btn.remove(); KotlinPlayground(codeDiv);
+  };
 }
 
 document.addEventListener("DOMContentLoaded", () => document.querySelectorAll(".literateBegin").forEach(enableCodeFilter));
