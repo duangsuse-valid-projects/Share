4e8886f61b1d995956d347be56048bb0d6e42486
duangsuse <fedora-opensuse@outlook.com>
2020-12-01 21:52:47 +0800

    Others(short_snake): Add more gui opts

--- Others/snakes/short_snake.c
+++ Others/snakes/short_snake.c
@@ -27,13 +27,14 @@ const int
   NO_LENDEC=0x2,
   USE_TRANSWALL = 0x4,
   USE_AI_L2=0x8,
-  NO_RESET=0x10;
-const int SNK_AI = 0x1;
-const int ARY_FLAG[] = {-1, 0x1, 0x2, 0x4, 0x8, 0x10, -1, -1};
-char* TEXT_MENU_PAUSE[] = {"resume game", "-walls (require reset&bug)",
+  NO_RESET=0x10,
+  USE_DELAY=0x20;
+const int SNK_AI = 0x1, SNK_PAUSE = 0x2;
+const int ARY_FLAG[] = {-1, 0x1, 0x2, 0x4, 0x8, 0x10, -1, -1, 0x20, -1};
+char* TEXT_MENU_PAUSE[] = {"resume game", "-walls (adds reset&bug)",
   "-len decrement", "+transport wall", "+AI level 2", "+no reset",
-  "one more fruit", "reverse snake", "add AI player"};
-const int nTextMenuPause = 9;
+  "one more fruit", "reverse snake", "add AI player", "+use delay", "pause snake"};
+const int nTextMenuPause = 11;
 
 bool isPosNegSign(char c) {  return c=='+' || c=='-'; }
 // Game object
@@ -41,16 +42,18 @@ int w, h, nM, nBlk, dCycle=1;
 typedef int* map2D;
 typedef struct SnakeST {
   map2D cells; int iHead, iTail;
-  int p, d; int flags;
+  int p, d; int flags, nLifes;
+  char* name;
   struct SnakeST* prev;
 }* Snake;
 map2D m;
 useconds_t delayUsec;
 
 Snake snakez = NULL;
+int nSnakeLifes;
 void snakeInit(Snake self) {
   self->iHead = 0, self->iTail = 0;
-  self->d = 1;
+  self->d = 1; self->flags = 0;
   self->cells = malloc(sizeof(int)*nM);
 }
 void snakeAdd(Snake self) {
@@ -110,32 +113,46 @@ void putFruit() { m[randP()] = 2; }
 typedef enum { updOk=0, updRegame, updDie } UpdateRes;
 typedef char* mutCstr;
 typedef const char* cstr;
-int /*act*/no=0; //bad, no idea to refactor in C99.
+int asprintf(char **strp, const char *fmt, ...); //stdio.h
+int /*act*/act=0; //bad, no idea to refactor in C99.
 inlined UpdateRes handleUpdate(int ch, int* flags, Snake snk) {
   int y, x;
   bool noDec=(*flags&NO_LENDEC), useTWall=(*flags&USE_TRANSWALL);
+  Snake snk1; // add-player
   if (ch == ERR) {}
   handleDirNeg(w, KEY_UP, KEY_DOWN)
   handleDirNeg(1, KEY_LEFT, KEY_RIGHT)
   else if (ch == 'p') {
-    no = dialogAskChoose("Paused", nTextMenuPause, textMenuPause, 2);
+    char* sTitle;
+    asprintf(&sTitle, "Paused on \"%s\" (%d) ;0x%hd", snk->name, snk->nLifes, snk->flags);
+    act = dialogAskChoose(sTitle, nTextMenuPause, textMenuPause, 2);
+    free(sTitle);
 rehandle:
-    if (no == 0) return updOk;
-    else if (no == 6) {
-      putFruit(); if (!(*flags&NO_RESET)) { no = 5/*no-reset*/; goto rehandle; } 
-    } else if (no == 7) {
+    if (act == 0) return updOk;
+    else if (6 <= act&&act <= 10 ) switch (act) {
+    case 6:
+      putFruit(); if (!(*flags&NO_RESET)) { act = 5/*no-reset*/; goto rehandle; } 
+      break;
+    case 7:
       swapInt(&snk->iHead, &snk->iTail); snk->d = -snk->d; dCycle = -dCycle;
       snk->iHead-=dCycle;snk->iTail-=dCycle;
       snk->p+=snk->d*abs(snk->iTail-snk->iHead); return updOk; // NOTE: not tested
-    } else if (no == 8) {
-      Snake snk1 = malloc(sizeof(struct SnakeST));
-      snk->p = randP(); snk->flags = SNK_AI;
-      snakeInit(snk1); snakeAdd(snk1);
+    case 8:
+      snk1 = malloc(sizeof(struct SnakeST));
+      snakeInit(snk1);
+      snk1->p = randP(); snk1->flags = SNK_AI;
+      snk1->name = "AI#"; snk1->nLifes = nSnakeLifes;
       putCell(snk1);
+      snakeAdd(snk1);
       return updOk;
+    case 9:
+      nodelay(stdscr, (*flags&USE_DELAY));
+      break;
+    case 10:
+      snk->flags ^= SNK_PAUSE;
     } else {
-      *flags = *flags ^ ARY_FLAG[no]; // switcher.
-      char* ptrT = textMenuPause[no];
+      *flags = *flags ^ ARY_FLAG[act]; // switcher.
+      char* ptrT = textMenuPause[act];
       ptrT[0] = (ptrT[0] == '+')? '-'  : '+';
     }
     if (*flags&NO_WALLS) memset(m, 0, sizeof(int)*nM);
@@ -155,44 +172,10 @@ rehandle:
   putCell(snk);
   return updOk;
 }
-#define MESG_ROUND_N 8
-#define setMessage(m) message = m; dtMessage = MESG_ROUND_N
-void game(cstr style[], int flags) {
-  noecho(); curUse(nodelay); curUse(keypad); cbreak();
-  struct SnakeST msnk;
-  snakeInit(&msnk); snakeAdd(&msnk);
-  Snake snk = snakez;
-  int ch, y,x;
-  cstr message=NULL; int dtMessage = 0;
-regame:
-  if (!(flags&NO_RESET)) {
-    if (!(flags&NO_WALLS)) putWall();
-    putFruit();
-    msnk.p = pYX(h/2, w/2); putCell(&msnk);
-  }
-  while ((ch = getch()) != 'q') {
-    do {
-      UpdateRes res = handleUpdate(ch, &flags, snk);
-      if (res == updRegame) goto regame;
-      else if (res == updDie) {
-        if (snk == &msnk) goto gameover;
-        setMessage("A snake just died");
-        do { m[snk->cells[cycledInc(&snk->iTail)]] = 0; } while (snk->iTail != snk->iHead);
-        snk->p = randP(); //respawn
-      }
-    } while ((snk = snk->prev) != NULL);
-    snk = snakez;
-    for (int i=0; i<nM; i++) { yxP(i, &y,&x); mvprintw(y, x*nBlk, style[m[i]]); }
-    if (message != NULL) { mvprintw(w/3, h-h/4, "%s", message); }
-    if (dtMessage == 0) { message = NULL; } else { dtMessage--; }
-    refresh();
-    usleep(delayUsec);
-  } //^ main loop
-gameover:
-  snk = snakez; do { snakeFree(snk); } while ((snk = snk->prev) != NULL);
+cstr envOr(cstr s, mutCstr name) {
+  cstr res = getenv(name);
+  return (res != NULL)? res : s;
 }
-#include <string.h>
-#include <locale.h>
 int ienvOr(int n, mutCstr name) {
   cstr res = getenv(name);
   if (res == NULL) return n;
@@ -200,10 +183,55 @@ int ienvOr(int n, mutCstr name) {
   int parsed = strtol(res, &ptrEnd, 10);
   return (ptrEnd != res)?  parsed : n;
 }
-cstr envOr(cstr s, mutCstr name) {
-  cstr res = getenv(name);
-  return (res != NULL)? res : s;
+#define NO_MORE (nUpdated == 0 && snk->prev == NULL)
+void game(cstr style[], int flags) {
+  noecho(); curUse(nodelay); curUse(keypad); cbreak(); 
+  int ntMessage = ienvOr(24, "ntMessage");
+  struct SnakeST msnk;
+  msnk.name = "Python"; msnk.nLifes = nSnakeLifes;
+  snakeInit(&msnk); snakeAdd(&msnk);
+  Snake snk = snakez;
+  int ch, y,x, nUpdated;
+  char* message=NULL; int dtMessage = ntMessage;
+regame:
+  if (!(flags&NO_RESET)) {
+    if (!(flags&NO_WALLS)) putWall();
+    putFruit();
+    msnk.p = pYX(h/2, w/2); putCell(&msnk);
+  }
+  while ((ch = getch()) != 'q') {
+    UpdateRes res;
+    do {
+      nUpdated = 0;
+      if (!(snk->flags&SNK_PAUSE)) {
+        res = handleUpdate(ch, &flags, snk); nUpdated++;
+      } else if (NO_MORE) goto gameover; else { res = updOk; }
+      if (res == updRegame) goto regame;
+      else if (res == updDie) {
+        if (NO_MORE) goto gameover;
+        asprintf(&message, "A snake \"%s\" just died (%d)", snk->name, snk->nLifes); // lifes.
+        do { m[snk->cells[cycledInc(&snk->iTail)]] = 0; } while (snk->iTail != snk->iHead);
+        if (snk->nLifes != 0) {
+          snk->nLifes -= 1;
+          snk->p = randP(); putCell(snk); //respawn
+        } else { snk->flags |= SNK_PAUSE; }
+      }
+    } while ((snk = snk->prev) != NULL);
+    snk = snakez;
+    for (int i=0; i<nM; i++) { yxP(i, &y,&x); mvprintw(y, x*nBlk, style[m[i]]); }
+    if (message != NULL) {
+        mvprintw(w/3, h-h/4, "%s", message);
+        if (dtMessage == 0) { free(message); message = NULL; dtMessage = ntMessage; } else { dtMessage--; }
+    } //< msg
+    refresh();
+    usleep(delayUsec);
+  } //^ main loop
+gameover:
+  snk = snakez; do { snakeFree(snk); } while ((snk = snk->prev) != NULL);
 }
+#undef NO_MORE
+
+#include <locale.h>
 void envBool(int* flags, mutCstr name, int flag) {
   cstr res = getenv(name);
   bool use = (res != NULL)? (strcmp(res, "no") != 0) : false;
@@ -217,6 +245,7 @@ int main(void) {
   initscr(); curs_set(0/*invisible*/);
   int fl = 0; envBool(&fl, "noWalls", NO_WALLS); envBool(&fl, "useAI2", USE_AI_L2);
   envBool(&fl, "noDecSnake", NO_LENDEC); envBool(&fl, "useTranspWall", USE_TRANSWALL);
+  nSnakeLifes = ienvOr(3, "nSnakeLifes");
   init(ienvOr(30, "ncol"), ienvOr(35, "nrow"), ienvOr(100, "dt_ms")); game(deftStyle, fl);
   nodelay(stdscr, false); getch();
   return endwin();
