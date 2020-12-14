ab3bef290a400d0d9696444d12e577b236e0422a
duangsuse <fedora-opensuse@outlook.com>
2020-12-15 04:24:38 +0800

    Others(FillTemplate): FAILED refactor of CStringIO.ReadIter

--- Others/fill_template.py
+++ Others/fill_template.py
@@ -4,7 +4,7 @@ import re # using Regex as tokenizer
 from re import compile as Regex
 
 from sys import argv, stderr, exc_info
-from sys import version_info # choose CStringIO stream impl cfg.
+from sys import version_info, getfilesystemencoding # choose CStringIO stream impl cfg.
 import traceback
 
 from os import mkdir, chdir, path, getenv
@@ -79,12 +79,15 @@ def macro(d, name, params, body, srcpos):
     eprint(":: %s in %s (%s:%d)" %(ex.msg, name, _sumSrcPos(evalCaller), ex.offset+pfix))
 
 def joinAlsoPrint(*args): print(*args); return "".join(args)
-def firstOccur(s, i, *texts):
-  iFound = -1
+def takeTillFirstOccur(cs, *texts):
+  s = str(cs)[cs.pos:]; iFound = -1
   for text in texts:
-    idx = s.find(text, i)
+    idx = s.find(text)
     if idx != -1 and (iFound == -1 or idx < iFound): iFound = idx
-  return iFound
+  i1 = 0
+  if iFound == -1: cs.io.seek(0, 2); i1 = cs.pos; cs.io.close() # EOF
+  else: i1 = iFound; cs.pos += i1+1
+  return s[:i1]
 
 def eprintSyntaxError(s, i, msg):
   slErr = list(s)
@@ -96,60 +99,77 @@ class CStringIO:
     global StringIO; from cStringIO import StringIO
   else: global StringIO; from io import StringIO
 
-  def __init__(self): self.sb = StringIO()
-  def write(self, s): self.sb.write(s)
-  def __str__(self): return self.sb.getvalue()
-  def clear(self): self.sb.seek(0); self.sb.truncate(0)
+  def __init__(self, initial=""): self.io = StringIO(initial); self.initial = initial
+  def write(self, s): self.io.write(s)
+  def __str__(self): return self.io.getvalue()
+  def clear(self): self.io.seek(0, 0); self.io.truncate(0)
   @property
-  def pos(self): return self.sb.tell()
+  def pos(self): return self.io.tell()
   @pos.setter
-  def setPos(self, i): self.sb.seek(i)
+  def pos(self, i): self.io.seek(i, 0)
+  def __iter__(self): return CStringIO.ReadIter(self)
+  class ReadIter:
+    def __init__(self, s): #dytype
+      self.s = CStringIO(unicode(s, getfilesystemencoding())) if version_info.major == 2 else s
+      self.pos = 0
+    def __next__(self):
+      c = self.s.io.read(1)
+      if c == "": self.s.io.close(); raise StopIteration()
+      self.pos += 1
+      return c
+    def skip(self, n):
+      self.pos += n
+      self.s.io.seek(self.s.io.tell()+n) # can't use SEEK_CUR on non-rb stream, see Issue 12922
+    def isEnd(self):
+      try:
+        oldPos = self.s.pos
+        b = not self.s.io.readable() or self.s.io.read(1) == ""
+        if not b: self.s.pos = oldPos
+        return b
+      except ValueError: return True
 
 RE_MACRO = Regex("(#|/{2})(\\S+?)\\(\\s*(.*?)\\)", re.DOTALL)
-def _expandMacroTo(sb, s, i0, get_subst):
+def _expandMacroTo(sb, cz, get_subst):
   #return RE_MACRO.sub(lambda m: get_subst(m[2], m[3]), s)
-  n = len(s); buf = CStringIO()
-  i = i0; state = 0
+  buf = CStringIO(); state = 0
   def appendBuf():
     sBuf = str(buf); buf.clear()
     if sBuf != "": sb.append(sBuf)
-    return i
-  while i < n:
-    c = s[i]
+  while True:
+    try: c = next(cz)
+    except StopIteration: break
     if state == 0 and c in "#/)": # state machine
-      if c == '#': buf.write('#'); i+=1
-      elif c == '/': buf.write('//'); i+=2
+      if c == '#': buf.write('#'); cz.skip(1)
+      elif c == '/': buf.write('//'); cz.skip(2)
       elif c == ')': return appendBuf()
       state = 1; continue # just like "call a function"
     elif state == 1:
       try:
-        iBuf0 = buf.pos
-        while not (s[i].isspace() or s[i] in "()#/"): buf.write(s[i]); i += 1
-        if s[i] == ')': return appendBuf()
+        c1 = c; iBuf0 = buf.pos
+        while not (c1.isspace() or c1 in "()#/"): buf.write(c1); c1 = next(cz)
+        if c1 == ')': return appendBuf()
         if buf.pos == iBuf0: appendBuf(); state = 0
-      except IndexError: break
-      if s[i] == '(':
+      except StopIteration: break # optimized
+      if c1 == '(':
         name = str(buf); buf.clear()
         iBeg = len(sb)
-        i = _expandMacroTo(sb, s, i+1, get_subst)
+        cz.skip(1); _expandMacroTo(sb, cz, get_subst)
         iEnd = len(sb)
         for idx in range(iBeg, iEnd): buf.write(sb.pop(iBeg))
         called = get_subst(name.lstrip("#/"), str(buf) )
         sb.insert(iBeg, called) # Excel-like step-by-step
         buf.clear()
-        if i >= n or s[i] != ')':
-          eprintSyntaxError(s, i, "expecting ')'")
+        if cz.isEnd() or next(cz) != ')':
+          eprintSyntaxError(cz.s.initial, cz.pos, "expecting ')'")
           break
-        i += 1
-    oldI = i
-    i = firstOccur(s, i, "#", "//", ")")
-    if i == -1: i = n # at EOF
-    skip = s[oldI:i]
+        cz.skip(1)
+    skip = takeTillFirstOccur(cz.s, "#", "//", ")")
     if skip != "": sb.append(skip)
+    if cz.isEnd(): break # at EOF
   return appendBuf() # just for fun, not serious.
 
 def expandMacro(s, get_subst):
-  sb = []; _expandMacroTo(sb, s, 0, get_subst)
+  sb = []; _expandMacroTo(sb, iter(CStringIO(s)), get_subst)
   return "".join(sb)
 
 
@@ -187,10 +207,10 @@ class FillTemplate:
       eprint("unknown command %s, treating as text" %fpOut)
       fpOut = ""; isText = True # notify '!'!
     fpOut1 = prefixes[0]+fpOut
-    if fpOut1 == "": return # talk-about is not for outer blocks
     code1 = expandMacro(code, self.getMacroResult)
     sBrief = "...".join(strBrief(RE_WHITE.sub(" ", code1), self.nPrevi))
     print("%s N=%i %s" %(fpOut1, len(code1), sBrief))
+    if fpOut1 == "": return # talk-about is not for outer blocks
     mkdirs(fpOut1)
     with open(fpOut1, self.writeMode) as fd:
       if isText and sCmt != None: fd.write(sCmt) # for "##"
