5fe3477e4359c901e45be6148e3412a517a9b8c2
duangsuse <fedora-opensuse@outlook.com>
2021-05-16 22:11:48 +0800

    HTMLs(school): BigOrange done

--- HTMLs/大橘子/emet.js
+++ HTMLs/大橘子/emet.js
@@ -2,7 +2,7 @@ String.prototype.splitCond=function(sep,op1,op2,limit){ limit=limit||0;
 	let iSep=this.indexOf(sep), ss0=this.slice(0,iSep),ss1=this.slice(iSep+1);
 	return (iSep==-1)? op1(this) : (limit==2)? op2(ss0,ss1) : op2(ss0, ...ss1.split(sep,limit-1))
 }
-function emetAttr(e,s_attrs) {
+function emetAttr(e,s_attrs) { // TODO support JSON str
   function setK(k,v) { v=v||"";
     if(k=="text") e.textContent=v; else e.setAttribute(k,v);
   }
@@ -13,11 +13,17 @@ function emet_(css_single) {
 	let iK1 = 0, vK1; // of last scanned marker
 	function onMarker(iK,v) {
 		if (iK1 == 0) {
-			e = document.createElement(s.slice(0,iK)); // tagName first.
+			let name=s.slice(0,iK); // support # tags and div omit.
+			if (s[0]=="#") {
+				let part = s.substr(1).split(":",2);
+				e = document["create"+emet.specialTag[part[0]]].call(document,part[1]||"");
+				return;
+			} else if(name=="") name="div";
+			e = (!!emet.ns)? document.createElementNS(emet.ns,name) : document.createElement(name); // tagName first.
 		} else {
-			if (vK1 instanceof String)e[vK1] = s.slice(iK1,iK);
+			if (typeof vK1=="string")e[vK1] = s.slice(iK1,iK); // sets-prop cond.
 		}
-		if(!(v instanceof String)) { // this cond can't be merged
+		if(typeof v!="string") { // this cond can't be merged
 			let iStop = s.indexOf(v[0], iK1);
 			v[1](e,s.slice(iK+1, iStop));
 		}
@@ -34,3 +40,7 @@ function emet(css) {
 	})
 }
 emet.ssyms=Object.entries({".": "className", "#": "id", "[":["]",emetAttr]});
+emet.ns=null;
+emet.specialTag={text:"TextNode",cmt:"Comment",cdata:"CDATASection",frag:"DocumentFragment",preproc:"ProcessingInstruction",attr:"Attribute"};
+
+// 下版分支支持 ,+ 操作符所需的 splitCond 子串末引解析，和能做 text/elem foreach${} 宏的模板系统一道带$变量
--- HTMLs/大橘子/util.js
+++ HTMLs/大橘子/util.js
@@ -7,10 +7,16 @@ Array.prototype.mapAppend=function(e0,op){
   for (let x of this) e0.appendChild(op(x));
   return e0
 }
+Array.prototype.partition=function(p){
+  let a=[],b=[];
+  for(let x of this)(p(x)?a:b).push(x);
+  return [a,b];
+}
 
 Object.mapValues_=function(o,op){
   for (let k of Object.keys(o)) o[k]=op(o[k]);
 }
+function forEntries(o,op){for(let [k,v] of Object.entries(o))op(k,v);}
 function copy(o){return o}//TODO
 function enable_(o) {
   for (let k in o) if(k[k.length-1]=="_") {
@@ -26,30 +32,41 @@ function ifCatch(op_cond, a,b) {
   try { op_cond(); return b; }
   catch (ex) { return a; }
 }
-enable_(Object)
+enable_(Object);
 
 String.prototype.replaceLazy=ifCatch(()=> assertEq("axbc","abc".replace("bc",k=>"x"+k)), function(s,get_s1){
   let i=this.indexOf(s);
   return (i==-1)? this : this.replace(s,get_s1(s));
-},String.prototype.replace)
-function dateFmt(fmt) {
-  function subst(op) {
-    let acc=fmt; // bad, improve with RegExp Symbol. replace/split
+},String.prototype.replace);
+
+function dateFmt(fmt) {//TODO 支持 :simp
+  function fillZ(n){return n<10?"0"+n:String(n)}
+  if(!Symbol.replace)function subst(op) {
+    let acc=fmt;
     for (let k in dateFmt.toks) acc = acc.replaceLazy(k, op);
     return acc
+  }else function subst(op){ // 丑.
+    return dateFmt.toks.re[Symbol.replace](fmt, (m1)=>{ return m1&&m1.length==2&&(m1[0]==m1[1])? fillZ(op(m1[0])):op(m1); })
   }
   let ks = [], re = RegExp(subst(k=>{ks.push(k);return "(.*?)"}), "g");
   return (t,s)=>{
     if(!!s) {
       let m = re.exec(s);
-      for(let i=1;i<m.length;i++) t["set"+dateFmt.toks[ks[i-1]] ].call(t, m[i]);
+      for(let i=1;i<m.length;i++) t["set"+dateFmt.toks[ks[i-1]] ].call(t, m[i]); // 就是以 m=[0,...ms] 为中心去迭代 i
+      return t;
     }
     return subst(k=>t["get"+dateFmt.toks[k]].call(t));
   };
 }
 dateFmt.toks={
-  "yyyy": "FullYear", "yy":"Year2", "mm": "Month", "dd": "Date",
-  "hh": "Hours", "MM": "Minutes","ss":"Seconds"};
+  "yyyy": "FullYear", "yy":"Year2", "m": "Month", "d": "Date",
+  "h": "Hours", "M": "Minutes","s":"Seconds",
+  get re(){
+    let ks=Object.keys(this); ks.splice(ks.indexOf("re"));
+    let [cOne,cs] = ks.partition(k=>k.length==1);
+    return RegExp(`[${cOne.join("")}]{1,2}|`+cs.map(c=>`(${c})`).join("|"), "g")
+  }
+}; // 不支持 p/am 和 week
 Object.assign(dateFmt, Object.mapValues({
   cnAll:"yyyy年mm月dd日hh时MM分ss秒",
   cnDay:"mm月dd日",
@@ -57,35 +74,50 @@ Object.assign(dateFmt, Object.mapValues({
   cnSimp:"mm月dd日 hh:MM",
   all: "yyyy-mm-dd hh:MM:ss",
   simp: "mm-dd hh:MM"
-}, dateFmt))
+}, dateFmt));
 
 function timerRate(dt_ms, op) {
   var id,opAgain;
   (function(){ if(!op())id=setTimeout(opAgain||(opAgain=arguments.callee.bind(null,arguments)), dt_ms);})();
   return function cancel(){clearTimeout(id)};
 }
+const timedToggle=(dt_ms,k)=>(ev)=>{
+  let evt=ev.target; evt[k]=true;
+  setTimeout(()=>{evt[k]=false},dt_ms);
+};
 function newArray(n,get){var a=Array(n),i;for(i=0;i<n;i++)a[i]=get(i);return a}
+String.prototype.extractFlag=function(s_pre){
+  return this.startsWith(s_pre)? [true,this.substr(s_pre.length)]:[false,this];
+}
+const rand=(a,b)=>Math.random()*b%a,randInt=(a,b)=>Math.floor(Math.random()*b)%a;
 
-const reParens=/\((.*?)\)/g;
-function elemMatchMapper(fmt,op_ks) {
-  let ks = [], re = RegExp(""+reParens[Symbol.replace](fmt, (m0,k)=>{ ks.push(k); return "(.*?)"; })+"", "g"); //FIXME
-  return (e0,s) => {
+const reParens=/\((.*?)\)/g,reVar=/%(.*?)%/g;
+function elemMatchMapper(fmt,op_ks=(k,s)=>{ let e=emet_(k);e.textContent=s; return e }) {
+  let [isAllFind,sFmt] = fmt.extractFlag("?");
+  let ks=[], nIncs=[];
+  let sre = reVar[Symbol.replace](sFmt, (m0,k)=>k.splitCond("|",
+    k=>{ks.push(k); return "(.*?)"},
+    (ssre,n_inc,k)=>{nIncs.push(parseInt(n_inc)); ks.push(k); return ssre}, 3)
+  ),
+  re = RegExp(isAllFind? sre : "^"+sre+"$",isAllFind?"g":"");
+  let search = isAllFind? s=>[...re[Symbol.matchAll](s)] : s=>reGroupAsMatch(re.exec(s));
+  return window.cell=(e0,s) => {
     var i1 = 0; // of last span
-    //let m0 = re.exec(s);
-    for (let m of re[Symbol.matchAll](s)) {
+    search(s).forEach((m,idx)=> { // 天哪咱竟然习惯了 JS Array+input&index RegMatch 格式
       let raw=s.slice(i1,m.index); if(raw!="") e0.append(raw);
-      i1 = m.index+(m[0].length-1); // 天哪咱竟然习惯了 JS Array+input&index RegMatch 格式
-      for (let i=1;i<m.length; i++) e0.appendChild(op_ks( ks[i-1], m[i] )); // 就是以 m=[0,...ms] 为中心去迭代 i
-    }
+      i1 = m.index+m[1].length+(nIncs[idx]||0); // 只是用 RE 提取句分，它index始终==0，正则不包含常量长信息，故此
+      e0.appendChild(op_ks( ks[idx], m[1] ));
+    });
     let raw=s.slice(i1); if(raw!="") e0.append(raw);
     return e0;
   }
 }
 function reGroupAsMatch(m) {
   if(m==null)return [];
-  var i0 = 0;
-  return newArray(m.length-1, i=>{ let s=m[1+i], idx=i0+i;i0+=s.length; return {0:m[0],1:s,index:idx,length:2} });
+  var i0 = m.index,idx;
+  return newArray(m.length-1, i=>{ let s=m[1+i], idx=i0;i0+=s.length; return {0:m[0],1:s,index:idx+i,length:2} });//FIXME why+i
 }
+
 
 function enableStyleSetprop() {
   var k,s;
@@ -97,3 +129,35 @@ function enableStyleSetprop() {
     }
   }
 }
+
+function enableClock(e){
+  let k=e.getAttribute("fmt"), fmt = k[0]==":"? dateFmt[k.substr(1)] : dateFmt(k);
+  enableClock.all.push(timerRate(1000,()=>{
+    e.textContent = fmt(new Date);
+  }))
+}
+enableClock.all=[];
+
+function expandTable(e0,code) {
+  let cell=elemMatchMapper(e0.getAttribute("gen"));
+  code.split("\n").mapAppend(e0, row=>row.split("|").mapAppend(emet("tr"), col=>
+    cell(emet("td"), col)
+  ));
+}
+function expandForm(e0,code) {
+  code=code||e0.firstChild.textContent.trim();
+  code.split("\n").mapAppend(e0, sInp=>{
+    let [k,t]=sInp.split(":",2);
+    let e= emet(`label input[name=${k}${k.endsWith("At")?",type=datetime-local":""}]`);
+    e[1].placeholder=t;e[0].insertBefore(document.createTextNode(t+"："),e[0].firstChild); return e[0];
+  })
+}
+function expandsPropMap(k,on_each,map){
+  function fmter(k,v){
+    let a = expandsPropMap.fmtRe.find(a=>a[0].test(k));
+    return (!!a)? a[1](v) : String(v)
+  }
+  if(typeof on_each=="string")on_each=qsEach.bind(null,on_each);
+  on_each(e=>{ e[k]=fmter(e[k],map.get(e[k])); })
+}
+expandsPropMap.fmtRe=[];
